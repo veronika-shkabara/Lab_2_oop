3480b0e196defe999746ceafc3d3c1b8a798276d
Вероніка <shkabara159@gmail.com>
2026-03-16 17:36:27 +0200

    git  commit -m

--- triangle.cpp
+++ triangle.cpp
@@ -7,7 +7,7 @@ double distance(Point a, Point b)
     return sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y));
 }
 
-// площа за формулою Герона
+
 double areaHeron(Point a, Point b, Point c)
 {
     double ab = distance(a,b);
