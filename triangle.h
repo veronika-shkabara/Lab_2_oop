05cee3908860663678d9033daa0233bcb20f6563
veronika-shkabara <VERONIKA.SHKABARA@lnu.edu.ua>
2026-03-23 19:41:45 +0200

    triangle.h

--- triangle.h
+++ triangle.h
@@ -7,21 +7,20 @@ struct Point {
     double x, y;
 };
 
-// Відстань між точками
+
 double distance(const Point& p1, const Point& p2);
 
 struct Triangle {
     Point A, B, C;
 
-    double shoelaceArea() const;  // площа по шнуруванню
-    double area() const;           // площа по Герону
-
+    double shoelaceArea() const;  
+    double area() const;           
     bool onBorder(const Point& P) const;
     bool contains(const Point& P) const;
     bool containsHeron(const Point& P) const;
 };
 
-// Функції для роботи з трикутником
+
 double heronArea(const Triangle& t);
 bool Vyrodzhenyi(const Triangle& t);
 
